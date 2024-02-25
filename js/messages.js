fd67614077c4885cdd61fe4072829d2e54d7962b
kaustav2610 <kastav2610@gmail.com>
2024-02-25 13:24:55 +0530

    first commit

---
+++ js/messages.js
@@ -0,0 +1,54 @@
+var messages = {
+  languageActive: {
+    'en': 'English'
+  },
+  next: {
+    'en': 'Next'
+  },
+  
+  labelLevel: {
+    'en': 'Level'
+  },
+  labelOf: {
+    'en': 'of'
+  },
+  title: {
+    'en': 'Learn to Flex - A game for learning CSS flexbox'
+  },
+  labelReset: {
+    'en': 'Reset'
+  },
+  warningReset: {
+    'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.'
+  },
+  gridGarden: {
+    'en': 'Want to learn CSS grid? Play'
+  },
+  labelLanguage: {
+    'en': 'Language'
+  },
+  labelSettings: {
+    'en': 'Settings'
+  },
+  labelDifficulty: {
+    'en': 'Difficulty'
+  },
+  labelColorblind: {
+    'en': 'Colorblind Mode'
+  },
+  labelColorblindOff: {
+    'en': 'Off'
+  },
+  labelColorblindOn: {
+    'en': 'On'
+  },
+  labelDifficultyEasy: {
+    'en': 'Beginner'
+  },
+  labelDifficultyMedium: {
+    'en': 'Intermediate - No Directions'
+  },
+  labelDifficultyHard: {
+    'en': 'Expert - No Directions & Random Levels'
+  }
+};
